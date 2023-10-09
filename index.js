f457529be32665d09e64c9de08a16302ec5d4da4
Jeffmwai <Jefylionel@gmail.com>
2023-10-09 17:30:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+// Define the saturdayFun function
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Define the mondayWork function
+  function mondayWork(task = 'go to the office') {
+    return `This Monday, I need to ${task}.`;
+  }
+  
+  // Define the wrapAdjective function
+  function wrapAdjective(flair = '*') {
+    return function(adjective = 'special') {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
+  // Example usage of wrapAdjective
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  
+  // Test case for wrapAdjective
+  let result = wrapAdjective("||");
+  let emphatic = result("a dedicated programmer");
+  
+  function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  }
+  console.log(emphatic); // Output: "You are ||a dedicated programmer||!"
+  
